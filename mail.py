96700296676e4456c273cb2ca6ac2ec8d1e7efcd
Muhammad Firdaus <firdauskoder@gmail.com>
2017-08-18 21:57:15 +0700

    Update mail.py

--- mail.py
+++ mail.py
@@ -4,11 +4,10 @@ import smtplib
 
 def send_forgot(receiver):
 	sender = "your@mail.dk"
-
 	inbox = """
-			Your Username : fird0s
-			Your Password : yourpass
-			"""
+		Your Username : fird0s
+		Your Password : yourpass
+		"""
 	header = ["From: " + sender, "Subject: Reset Your Password", "To: " + receiver, "Content-Type: text/html", inbox]
 
 	try:
@@ -18,14 +17,9 @@ def send_forgot(receiver):
 		session.login('firdaus@jones.dk', '')
 		session.sendmail(sender, receiver, header)
 		session.quit()
-	finally:
-		print "ok"
-		
-		
-		
-		
+	finally:		
 		datauser.email = request.form["email"]	
-			datauser.angkatan = request.form["angkatan"]	
-			datauser.handphone = request.form["hp"]	
-			datauser.work = request.form["pekerjaan"]	
-			datauser.status = request.form["status"]	
+		datauser.angkatan = request.form["angkatan"]	
+		datauser.handphone = request.form["hp"]	
+		datauser.work = request.form["pekerjaan"]	
+		datauser.status = request.form["status"]	
